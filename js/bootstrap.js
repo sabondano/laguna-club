cc881837e64fd13fa61e925c2723afc2d7674757
Sebastian Abondano <sabondano1@gmail.com>
2014-09-13 23:04:59 -0400

    added navbar partial and replaced it with navbar code that was on all pages, updated products.html layout accordingly, and added js to dynamically assign active class to the correct navbar item

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -2112,3 +2112,31 @@ if (typeof jQuery === 'undefined') { throw new Error('Bootstrap\'s JavaScript re
   })
 
 }(jQuery);
+
+
+// Customizations - After what came with bootstrap by default
+// ==========================================================
+
+/* Navigation Bar  - Assign active class to li if same as pathname
+*/
+$(document).ready(function() {
+  $(function(){
+    function stripTrailingSlash(str) {
+      if(str.substr(-1) == '/') {
+        return str.substr(0, str.length - 1);
+      }
+      return str;
+    }
+
+    var url = window.location.pathname;  
+    var activePage = stripTrailingSlash(url);
+
+    $('.nav li a').each(function(){  
+      var currentPage = stripTrailingSlash($(this).attr('href'));
+
+      if (activePage == currentPage) {
+        $(this).parent().addClass('active'); 
+      } 
+    });
+  });
+});
